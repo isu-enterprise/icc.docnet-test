50f41f4f53d8bacafd2aeb20c4f99b194b80f7d5
Evgeny Cherkashin <eugene@irnok.net>
2017-08-18 17:26:47 +0800

    Added data path location.

--- icc.docnet.tests/tests/test_basic.py
+++ icc.docnet.tests/tests/test_basic.py
@@ -7,6 +7,12 @@ from zope.component import getUtility
 
 from pyramid.paster import get_appsettings
 
+import pkg_resources
+import os.path
+
+DATA_PATH = pkg_resources.resource_filename('icc.docnet.tests', '../DATA')
+DATA_PATH = os.path.abspath(DATA_PATH)
+
 
 def configurator(global_config, **settings):
     config = app.configurator(global_config, **settings)
@@ -23,8 +29,11 @@ class TestBasic:
     def setUp(self):
         pass
 
-    def test_something(self):
+    def test_registry_works(self):
         assert getUtility(IApplication, name="application") == application
+
+    def test_import_file(self):
+        assert 0
 
     def tearDown(self):
         pass
