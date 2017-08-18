633e5ace3f6346f5510dcd10d5a0bfbcb99ab573
Evgeny Cherkashin <eugene@irnok.net>
2017-08-18 17:15:36 +0800

    First setup to be tested

--- icc.docnet.tests/tests/test_basic.py
+++ icc.docnet.tests/tests/test_basic.py
@@ -1,5 +1,25 @@
 from nose.plugins.skip import SkipTest
 from nose.tools import assert_raises, nottest
+from pyramid.config import Configurator
+from zope.component import getGlobalSiteManager
+from zope.interface import directlyProvides
+from isu.webapp.interfaces import IConfigurationEvent, IApplication
+
+from isu.webapp import app
+
+from isu.enterprise.configurator import createConfigurator
+
+from pyramid.paster import get_appsettings
+
+
+def configurator(global_config, **settings):
+    config = app.configurator(global_config, **settings)
+    return app.create_application(config)
+
+
+settings = get_appsettings('icc.cellula-test.ini', name='main')
+application = configurator(settings)
+
 
 #@SkipTest
 class TestBasic:
