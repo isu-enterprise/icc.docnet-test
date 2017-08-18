e0fbd7a1154381623c78c45cdae0d62c6f86c46d
Evgeny Cherkashin <eugene@irnok.net>
2017-08-18 18:00:10 +0800

    Dancing with source data.

--- icc.docnet.tests/tests/test_basic.py
+++ icc.docnet.tests/tests/test_basic.py
@@ -6,34 +6,50 @@ from isu.webapp import app
 from zope.component import getUtility
 
 from pyramid.paster import get_appsettings
+from icc.cellula.tasks import DocumentAcceptingTask
 
 import pkg_resources
 import os.path
 
-DATA_PATH = pkg_resources.resource_filename('icc.docnet.tests', '../DATA')
+DATA_PATH = pkg_resources.resource_filename(
+    'icc.docnet.tests', '../../../../../DATA')
 DATA_PATH = os.path.abspath(DATA_PATH)
+SRC = os.path.join(DATA_PATH, "source")
 
 
 def configurator(global_config, **settings):
     config = app.configurator(global_config, **settings)
     return app.create_application(config)
+    pass
 
 
 settings = get_appsettings('icc.cellula-test.ini', name='main')
 application = configurator(settings)
 
 
+def bookname(name):
+    return os.path.join(SRC, name)
+
 #@SkipTest
+
+
 class TestBasic:
 
     def setUp(self):
-        pass
+        self.book1name = "French_ways_and_their_Meaning-Wharton.pdf"
+        self.book1 = bookname(self.book1name)
 
     def test_registry_works(self):
         assert getUtility(IApplication, name="application") == application
 
     def test_import_file(self):
-        assert 0
+        assert self.book1.endswith("pdf")
+
+    def test_book_processing(self):
+        book = self.book1
+        headers = {"File-Name": self.book1name}
+        content = open(book, "rb").read()
+        DocumentAcceptingTask(content, headers).enqueue(block=False, view=None)
 
     def tearDown(self):
         pass
